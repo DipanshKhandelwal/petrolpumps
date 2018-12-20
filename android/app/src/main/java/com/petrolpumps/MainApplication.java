d146dbe5d540131432b898e8e2ca285529a9952d
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-12-20 16:39:17 +0530

    Add react-native-maps

--- android/app/src/main/java/com/petrolpumps/MainApplication.java
+++ android/app/src/main/java/com/petrolpumps/MainApplication.java
@@ -3,6 +3,7 @@ package com.petrolpumps;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new RNGestureHandlerPackage(),
           new FBSDKPackage(mCallbackManager)
       );
