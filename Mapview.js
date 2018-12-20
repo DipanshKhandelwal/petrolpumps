2e5a7191ec4c69734525a7a98f34d875d098efb9
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-12-20 17:27:01 +0530

    Add markers to map

--- Mapview.js
+++ Mapview.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import _ from 'lodash';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 class Mapview extends Component {
 
@@ -11,18 +12,33 @@ class Mapview extends Component {
     };
 
     render(){
+
+        var venues = this.props.navigation.getParam('venues', [])
+
         return(
             <View style={styles.container}>
                 <MapView
                 provider={PROVIDER_GOOGLE}
                 style={styles.map}
                 region={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
+                    latitude: venues[0].location.lat,
+                    longitude: venues[0].location.lng,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
                 }}
                 >
+                {
+                    _.map(venues, (venue) => 
+                        <Marker
+                            key={venue.key}
+                            coordinate={{
+                                latitude: venue.location.lat,
+                                longitude: venue.location.lng
+                            }}
+                            title={venue.name}
+                        />
+                    )
+                }
                 </MapView>
             </View>
         );
