ea468df54c4419b228b7ee449fac7162caeafbdf
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-12-20 14:47:57 +0530

    Add a search bar

--- PumpsList.js
+++ PumpsList.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import _ from 'lodash';
+import { View, Text, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native';
+const axios = require('axios');
+import Login from './Login';
 
 class PumpsList extends Component {
 
@@ -9,13 +12,27 @@ class PumpsList extends Component {
       };
   };
 
+  state = {
+    text : "",
+    list: null,
+    response: null
+  }
+
+  _onTextChange = (text) => {
+    this.setState({ text });
+  }
+
   render() {
     return(
       <View style={{ backgroundColor: '#F5FCFF', display: 'flex', flex: 1 }} >
-        <Text>
-          PumpsList
-        </Text>
-      </View>
+        <View style={{ backgroundColor: '#b9b9b9', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', elevation: 3 }} >
+          <Text style={{ padding: 10, margin: 10, fontWeight: 'bold', fontSize: 15 }} >Search</Text>
+          <TextInput
+            style={{ backgroundColor: '#F5FCFF', display: 'flex', flex: 1, padding: 5, marginRight: 5 }}
+            onChangeText={(text) => this._onTextChange( text )}
+            />
+          <Button title="Go" style={{ marginRight: 10 }} />
+        </View>
     );
   }
 }
